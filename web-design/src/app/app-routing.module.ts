de9c0a78c348206eb4828ac7dfdef687339ef982
RETprojects <remitoutin@gmail.com>
2022-01-07 20:20:49 -0600

    fixed the EventsComponent error

--- web-design/src/app/app-routing.module.ts
+++ web-design/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { EventsComponent } from './events/events.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { EventsComponent } from './events/events.component';
 
