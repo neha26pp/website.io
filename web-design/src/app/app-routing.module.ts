cd908df70448d4d6a60bf0f762bd6f62001f185d
RETprojects <remitoutin@gmail.com>
2022-01-07 20:02:22 -0600

    solved EventsComponent error

--- web-design/src/app/app-routing.module.ts
+++ web-design/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
 import { CalendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'aboutus', component: AboutUsComponent},
-  // {path: 'events', component: EventsComponent},
+  {path: 'events', component: EventsComponent},
   {path: 'calendar', component: CalendarComponent},
   { path: '', redirectTo: '/aboutuspage', pathMatch: 'full' },
 
