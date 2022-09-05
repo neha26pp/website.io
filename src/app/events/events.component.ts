7fa0ff4e999b858add2f13fbc8c7093ca39302d4
Manasi Patil <manasispatil19@gmail.com>
2022-09-05 17:26:12 -0400

    Removed the toggle features and instructions for adding new events from the sidebar and hardcoded some events in the events page

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/angular';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi, CalendarApi } from '@fullcalendar/angular';
 import { INITIAL_EVENTS, createEventId } from './event-utils';
+import { EventInput } from '@fullcalendar/angular';
+
+export const eventID:string[] = [];
+export const eventTitle:string[] = [];
+export const eventStart:string[] = [];
+export const eventEnd:string[] = [];
+export const eventAllDay:boolean[] = [];
 
 @Component({
   selector: 'app-events',
@@ -53,25 +60,40 @@ export class EventsComponent implements OnInit {
 
     calendarApi.unselect(); // clear date selection
 
-    if (adminName == 'neha') {
+    if (adminName == 'neha' || adminName == 'manasi') {
       const title = prompt('Enter event title')
       if(title){
           //const eventTitle = prompt('Enter the event title:')
+          const eventId = createEventId();
         calendarApi.addEvent({
-        id: createEventId(),
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay
-      });
+          id: eventId,
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay
+        });
+        eventID.push(eventId);
+        eventStart.push(selectInfo.startStr);
+        eventEnd.push(selectInfo.endStr);
+        eventAllDay.push(selectInfo.allDay);
+        // INITIAL_EVENTS.push(calendarApi.);
+        // INITIAL_EVENTS.push({
+        //   id: eventId,
+        //   title,
+        //   start: selectInfo.startStr,
+        //   end: selectInfo.endStr,
+        //   allDay: selectInfo.allDay
+        // })
       }
-
+    }
+    else {
+      alert('You are not an admin so you don\'t get access')
     }
   }
 
   handleEventClick(clickInfo: EventClickArg) {
     const adminName = prompt('Enter admin name')
-    if (adminName == 'neha') {
+    if (adminName == 'neha' || adminName == 'manasi') {
       clickInfo.event.remove();
     }
   }
