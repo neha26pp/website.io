e9e6d279dc0af114711492f0b45bdc7c06c2367f
Manasi Patil <manasispatil19@gmail.com>
2022-09-05 21:56:09 -0400

    Leaderboard cards

---
+++ src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CommonModule } from '@angular/common';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { MatCardModule } from "@angular/material/card";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+//import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
+import { EventsComponent } from './events/events.component';
+import { FullCalendarModule } from '@fullcalendar/angular'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import interactionPlugin from '@fullcalendar/interaction'; // a plugin!
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid'
+
+FullCalendarModule.registerPlugins([
+  dayGridPlugin,
+  timeGridPlugin,
+  listPlugin,
+  interactionPlugin
+])
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutUsComponent,
+    EventsComponent,
+    HeaderComponent,
+    EventsComponent,
+    //CalendarComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    NgbModalModule,
+    MatCardModule,
+    FullCalendarModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
